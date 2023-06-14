44ba74c256c3d5d6da0e0f9fdacaed805947bf09
GRACIELAGON11 <133450979+GRACIELAGON11@users.noreply.github.com>
2023-06-14 17:12:18 -0600

    Update app.py
    Se agrego eliminar y guradar

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,10 +1,23 @@
+#paquteria de flas
 from flask import Flask
 
+#la inicializacion del app 
 app=Flask(__name__)
-
+#declaracion o inicializacion de las rutas y le pertenece a http://localhost:5000
 @app.route('/')
 def index():
         return "Hola mundo Flask"
 
+@app.route('/guardar')
+def guardar():
+    return "se guardo"
+
+@app.route('/eliminar')
+def eliminar():
+    return "se elimino "
+
+
+#ejecucion del servidor y asignacion del puerto a trabajar
 if __name__ == '__main__':
-        app.run(port=5000)
+        app.run(port=4000 ,debug= True)
+
