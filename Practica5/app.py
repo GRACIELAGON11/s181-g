3f511caad7d4011cf49beae1c8e5d4b3969f86d8
GRACIELAGON11 <133450979+GRACIELAGON11@users.noreply.github.com>
2023-06-28 16:43:32 -0600

    Se  creo una tabla para mostrar los datos guardados en la base de datos

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,5 +1,5 @@
 #paquteria de flas
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, redirect,url_for,flash
 from flask_mysqldb import MySQL
 
 #la inicializacion del app 
@@ -8,21 +8,32 @@ app=Flask(__name__)
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='dbflask'
+app.config['MYSQL_DB']='dbflas'
+app.secret_key='mysecretkey'
 MySQL=MySQL(app)
 #declaracion o inicializacion de las rutas y le pertenece a http://localhost:5000
 @app.route('/')
 def index():
-        return render_template('index.html')
+        cc=MySQL.connection.cursor();
+        cc.execute('select * from Albums')
+        #guardar consulta
+        conAlbums=cc.fetchall();
+        print(conAlbums)
+        return render_template('index.html',listalbums=conAlbums)
 @app.route('/guardar',methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo=request.form['txtTitulo']
-        artista=request.form['textArtista']
-        anio=request.form['txtAnio']
-        print(titulo,artista,anio)
-
-    return "Se guardo en la Base de Datos"
+        #pasamos a variables el contenido de los input
+        Vtitulo=request.form['txtTitulo']
+        Vartista=request.form['textArtista']
+        Vanio=request.form['txtAnio']
+    
+        #conectar y ejecutar el insert
+        CS= MySQL.connection.cursor()
+        CS. execute('insert into Albums(titulo,artista,anio) values(%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+        MySQL.connection.commit()
+    flash('EL ALBUM FUE AGREGADO EXITOSAMENTE')
+    return redirect(url_for('index'))
 
 @app.route('/eliminar')
 def eliminar():
